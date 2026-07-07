75fca17993a88322f2e1bfb2d48baaa3192e7aee
esouzazavala <esouza_zavala@costco.com>
2026-07-07 09:56:23 -0500

    Change default location to 'South Central US'

--- nd082-Azure-Cloud-DevOps-Starter-Code/C1 - Azure Infrastructure Operations/project/starter_files/Vars.tf
+++ nd082-Azure-Cloud-DevOps-Starter-Code/C1 - Azure Infrastructure Operations/project/starter_files/Vars.tf
@@ -5,7 +5,7 @@ variable "prefix" {
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
-  default = "westeurope"
+  default = "South Central US",
 }
 
 variable "username" {
