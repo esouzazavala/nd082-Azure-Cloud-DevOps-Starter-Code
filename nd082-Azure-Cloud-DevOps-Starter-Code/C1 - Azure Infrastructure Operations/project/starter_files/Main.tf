a9dc729cccdee4764fe44f078ed8670cd41dd67e
esouzazavala <esouza_zavala@costco.com>
2026-07-20 13:00:31 -0500

    Refactor Azure Terraform configuration with variables
    Updated Azure Terraform configuration to use variables for client and resource group details, and adjusted resource definitions for network and virtual machines.

--- nd082-Azure-Cloud-DevOps-Starter-Code/C1 - Azure Infrastructure Operations/project/starter_files/Main.tf
+++ nd082-Azure-Cloud-DevOps-Starter-Code/C1 - Azure Infrastructure Operations/project/starter_files/Main.tf
@@ -1,203 +1,168 @@
 provider "azurerm" {
-  features {}
+    features {}
+    client_id       = "${var.client_id}"
+    client_secret   = "${var.client_secret}"
+    subscription_id = "${var.subscription_id}"
+    tenant_id       = "${var.tenant_id}"
 }
 
-# get the image that was create by the packer script
-data "azurerm_image" "web" {
-  name                = "udacity-packer-image"
-  resource_group_name = var.packer_resource_group
+// Resource group
+data "azurerm_resource_group" "main" {
+    name     = var.resourcegroup
 }
 
-# Create the resource group
-resource "azurerm_resource_group" "main" {
-  name     = "${var.prefix}-rg"
-  location = var.location
-}
-# create a virtual network
+// Virtual network
 resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/22"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-
-  tags = {
-    project = "udacity1"
-    environment = "development"
-  }
+    name                    = "${var.prefix}-network"
+    address_space           = ["10.0.0.0/22"]
+    location                = data.azurerm_resource_group.main.location
+    resource_group_name     = data.azurerm_resource_group.main.name
+    tags                    = var.tags
 }
 
-# Create the subnet
+// Internal subnet for the upper vnet
 resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
+    name                    = "internal"
+    resource_group_name     = data.azurerm_resource_group.main.name
+    virtual_network_name    = azurerm_virtual_network.main.name
+    address_prefixes        = ["10.0.2.0/24"]
 }
 
-# Create the network security group
+// Network security group
 resource "azurerm_network_security_group" "main" {
-  name                = "acceptanceTestSecurityGroup1"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-
-  tags = {
-    project = "udacity1"
-    environment = "development"
-  }
+    name                    = "${var.prefix}-nsg"
+    location                = data.azurerm_resource_group.main.location
+    resource_group_name     = data.azurerm_resource_group.main.name
+    tags                    = var.tags
 }
 
-# Create security rules
+// Allow only acces to VMs on the same subnet
 resource "azurerm_network_security_rule" "rule1" {
-    name                         = "DenyAllInbound"
-    description                  = "This rule with low priority deny all the inbound traffic."
-    priority                     = 100
-    direction                    = "Inbound"
-    access                       = "Deny"
-    protocol                     = "*"
-    source_port_range            = "*"
-    destination_port_range       = "*"
-    source_address_prefix        = "*"
-    destination_address_prefix   = "*"
-    resource_group_name          = azurerm_resource_group.main.name
-    network_security_group_name  = azurerm_network_security_group.main.name
+    name                       = "allow-internal"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "10.0.0.0/24"
+    destination_address_prefix = "*"
+    description                = "description-ssh"
+    resource_group_name         = data.azurerm_resource_group.main.name
+    network_security_group_name = azurerm_network_security_group.main.name
 }
 
+// Deny all access from the internet
 resource "azurerm_network_security_rule" "rule2" {
-    name                         = "AllowInboundInsideVN"
-    description                  = "This rule allow the inbound traffic inside the same virtual network."
-    priority                     = 101
-    direction                    = "Inbound"
-    access                       = "Allow"
-    protocol                     = "*"
-    source_port_ranges           = azurerm_virtual_network.main.address_space
-    destination_port_ranges      = azurerm_virtual_network.main.address_space
-    source_address_prefix        = "VirtualNetwork"
-    destination_address_prefix   = "VirtualNetwork"
-    resource_group_name          = azurerm_resource_group.main.name
-    network_security_group_name  = azurerm_network_security_group.main.name
+    name                       = "deny-internet"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "10.0.0.0/24"  
+    resource_group_name         = data.azurerm_resource_group.main.name
+    network_security_group_name = azurerm_network_security_group.main.name
 }
 
-resource "azurerm_network_security_rule" "rule3" {
-    name                         = "AllowOutboundInsideVN"
-    description                  = "This rule allow the outbound traffic inside the same virtual network."
-    priority                     = 102
-    direction                    = "Outbound"
-    access                       = "Allow"
-    protocol                     = "*"
-    source_port_ranges           = azurerm_virtual_network.main.address_space
-    destination_port_ranges      = azurerm_virtual_network.main.address_space
-    source_address_prefix        = "VirtualNetwork"
-    destination_address_prefix   = "VirtualNetwork"
-    resource_group_name          = azurerm_resource_group.main.name
-    network_security_group_name  = azurerm_network_security_group.main.name
-}
-
-resource "azurerm_network_security_rule" "rule4" {
-    name                         = "AllowHTTPFromLB"
-    description                  = "This rule allow the HTTP traffic from the load balancer."
-    priority                     = 103
-    direction                    = "Inbound"
-    access                       = "Allow"
-    protocol                     = "Tcp"
-    source_port_ranges           = azurerm_virtual_network.main.address_space
-    destination_port_ranges      = azurerm_virtual_network.main.address_space
-    source_address_prefix        = "AzureLoadBalancer"
-    destination_address_prefix   = "VirtualNetwork"
-    resource_group_name          = azurerm_resource_group.main.name
-    network_security_group_name  = azurerm_network_security_group.main.name
-}
-
-# Create network interface
+// Network interface group
 resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-
-  ip_configuration {
+    name                = "${var.prefix}-nic${count.index}"
+    location            = data.azurerm_resource_group.main.location
+    resource_group_name = data.azurerm_resource_group.main.name
+    count               = var.vm_count
+      ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
-  }
-
-  tags = {
-    project = "udacity1"
-    environment = "development"
-  }
+    }  
+    tags                          = var.tags
 }
 
-# Create public IP
+// Public IP
 resource "azurerm_public_ip" "main" {
-  name                = "${var.prefix}-public-ip"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  allocation_method   = "Static"
-
-  tags = {
-    project = "udacity1"
-    environment = "development"
-  }
+    name                = "${var.prefix}-pubip"
+    location            = data.azurerm_resource_group.main.location
+    resource_group_name = data.azurerm_resource_group.main.name
+    allocation_method   = "Dynamic"
+    sku                 = "Basic" 
+    tags                = var.tags
 }
 
-# Create load balancer
+// Load balancer
 resource "azurerm_lb" "main" {
-  name                = "${var.prefix}-lb"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+    name                = "${var.prefix}-lb"
+    location            = data.azurerm_resource_group.main.location
+    resource_group_name = data.azurerm_resource_group.main.name
 
-  frontend_ip_configuration {
-    name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.main.id
-  }
+    frontend_ip_configuration {
+        name                 = "${var.prefix}-load-balancer-frontend"
+        public_ip_address_id = azurerm_public_ip.main.id
+    }
+
+    tags                     = var.tags
 }
 
-# The load balancer will use this backend pool
+// Load balancer backend address pool
 resource "azurerm_lb_backend_address_pool" "main" {
-  resource_group_name = azurerm_resource_group.main.name
-  loadbalancer_id     = azurerm_lb.main.id
-  name                = "${var.prefix}-lb-backend-pool"
+    loadbalancer_id = azurerm_lb.main.id
+    name            = "backend-adress-pool"
 }
 
-# We associate the LB with the backend address pool
+// Load balancer backend network address pool association
 resource "azurerm_network_interface_backend_address_pool_association" "main" {
-  network_interface_id    = azurerm_network_interface.main.id
-  ip_configuration_name   = "internal"
-  backend_address_pool_id = azurerm_lb_backend_address_pool.main.id
+    count                   = var.vm_count
+    network_interface_id    = element(azurerm_network_interface.main.*.id, count.index)
+    ip_configuration_name   = "internal"
+    backend_address_pool_id = azurerm_lb_backend_address_pool.main.id
 }
 
-# Create virtual machine availability set
+// VM availability set
 resource "azurerm_availability_set" "main" {
-  name                = "${var.prefix}-aset"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-
-  tags = {
-    project = "udacity1"
-    environment = "development"
-  }
+    name                = "${var.prefix}-availset"
+    location            = data.azurerm_resource_group.main.location
+    resource_group_name = data.azurerm_resource_group.main.name
+    tags                = var.tags
 }
 
-# Create the virtual machines
+// Creation of virtual machines
+data "azurerm_image" "main" {
+    name            = "PackerImage"
+    resource_group_name = data.azurerm_resource_group.main.name
+}
+
 resource "azurerm_linux_virtual_machine" "main" {
-  count                           = var.nb_vms
-  name                            = "${var.prefix}-vm-${count.index}"
-  resource_group_name             = azurerm_resource_group.main.name
-  location                        = azurerm_resource_group.main.location
-  size                            = "Standard_B1ls"
-  admin_username                  = "${var.username}"
-  admin_password                  = "${var.password}"
-  disable_password_authentication = false
-  network_interface_ids = [element(azurerm_network_interface.main.*.id, count.index)]
-  availability_set_id = azurerm_availability_set.main.id
+    count                           = var.vm_count       
+    name                            = "${var.prefix}-vm${count.index}" 
+    resource_group_name             = data.azurerm_resource_group.main.name
+    location                        = data.azurerm_resource_group.main.location
+    size                            = "Standard_D2s_v3"
+    admin_username                  = "${var.username}"
+    admin_password                  = "${var.password}"
+    disable_password_authentication = false
+    availability_set_id             = azurerm_availability_set.main.id
+    network_interface_ids = [element(azurerm_network_interface.main.*.id, count.index),]
 
-  source_image_id = data.azurerm_image.web.id
+    os_disk {
+        storage_account_type = "Standard_LRS"
+        caching              = "ReadWrite"
+    }
 
-  os_disk {
+    tags                     = var.tags
+
+    source_image_id = data.azurerm_image.main.id
+}
+
+// Managed disks creation
+resource "azurerm_managed_disk" "main" {
+    count                = var.vm_count
+    name                 = "${var.prefix}-managed-disk-${count.index}"
+    location             = data.azurerm_resource_group.main.location
+    resource_group_name  = data.azurerm_resource_group.main.name
     storage_account_type = "Standard_LRS"
-    caching              = "ReadWrite"
-  }
-
-  tags = {
-    project = "udacity1"
-    environment = "development"
-  }
+    create_option        = "Empty"
+    disk_size_gb         = "1"
+    tags                 = var.tags
 }
